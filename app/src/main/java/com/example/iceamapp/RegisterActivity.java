eebbf135d4706dee2262fa51df8c233efdfb48f9
thanhtungt <tungccvv111@gmail.com>
2025-03-05 17:01:53 +0700

    layout login register animation

---
+++ app/src/main/java/com/example/iceamapp/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.iceamapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}

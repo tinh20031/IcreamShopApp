37c7c1aa6edad36d548d6efcfecee9bcf4f5ffcb
qe170202 <Quocdinhkiu@gmail.com>
2025-03-06 15:37:04 +0700

    update launch screen

--- app/src/main/java/com/example/iceamapp/fragments/HeaderFragment.java
+++ app/src/main/java/com/example/iceamapp/fragments/HeaderFragment.java
@@ -1,39 +1,30 @@
 package com.example.iceamapp.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-
+import com.example.iceamapp.fragments.CartActivity;
 import com.example.iceamapp.R;
 
 public class HeaderFragment extends Fragment {
-
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_header, container, false);
 
-        // Tìm các ImageView
+        // Xử lý khi nhấn vào giỏ hàng
         ImageView cartIcon = view.findViewById(R.id.cartIcon);
-        ImageView notificationIcon = view.findViewById(R.id.notificationIcon);
-        ImageView userAvatar = view.findViewById(R.id.userAvatar);
-
-        // Xử lý sự kiện nhấp
-        cartIcon.setOnClickListener(v -> loadContentFragment(new CartFragment()));
-        notificationIcon.setOnClickListener(v -> loadContentFragment(new NotificationFragment()));
-        userAvatar.setOnClickListener(v -> loadContentFragment(new UserFragment()));
+        cartIcon.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), CartActivity.class);
+            startActivity(intent);
+        });
 
         return view;
-    }
-
-    // Hàm thay thế Fragment nội dung
-    private void loadContentFragment(Fragment fragment) {
-        FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.contentFragmentContainer, fragment);
-        transaction.addToBackStack(null);
-        transaction.commit();
     }
 }

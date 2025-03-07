7a2630370ed7c53b1a7d54825492e20c50c68d2e
tinh <tinhtranquocx@gmail.com>
2025-03-08 00:24:15 +0700

    update retrofit

--- app/src/main/java/com/example/iceamapp/RetrofitClient.java
+++ app/src/main/java/com/example/iceamapp/RetrofitClient.java
@@ -7,7 +7,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static final String BASE_URL = "https://10.0.2.2:7283/"; // Đảm bảo đúng URL API của bạn
+    private static final String BASE_URL = "https://10.0.2.2:7283/";
     private static Retrofit retrofit = null;
 
     // Phương thức khởi tạo Retrofit chung

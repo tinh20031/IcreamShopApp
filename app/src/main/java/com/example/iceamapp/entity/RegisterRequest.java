00ab6818ccd1e2ec4fcb6d685184312ea5956d95
thanhtungt <tungccvv111@gmail.com>
2025-03-08 20:36:40 +0700

    done login register

---
+++ app/src/main/java/com/example/iceamapp/entity/RegisterRequest.java
@@ -0,0 +1,81 @@
+package com.example.iceamapp.entity;
+
+import com.google.gson.annotations.SerializedName;
+
+public class RegisterRequest {
+
+    @SerializedName("fullName")
+    private String fullName;
+
+    @SerializedName("email")
+    private String email;
+
+    @SerializedName("password")
+    private String password;
+
+    @SerializedName("phone")
+    private String phone;
+
+    @SerializedName("address")
+    private String address;
+
+    @SerializedName("role")
+    private String role;
+
+    public RegisterRequest(String fullName, String email, String password, String phone, String address, String role){
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.address=address;
+        this.role = role;
+    }
+
+    public String getFullName(){
+        return fullName;
+    }
+
+    public void setFullName(String fullName){
+        this.fullName = fullName;
+    }
+
+    public  String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public String getAddress(){
+        return address;
+    }
+
+    public void setAddress(String address){
+        this.address = address;
+    }
+
+    public String getPhone(){
+        return phone;
+    }
+
+    public void setPhone(String phone){
+        this.phone = phone;
+    }
+
+    public String getRole(){
+        return role;
+    }
+
+    public void setRole(String role){
+        this.role= role;
+    }
+}
